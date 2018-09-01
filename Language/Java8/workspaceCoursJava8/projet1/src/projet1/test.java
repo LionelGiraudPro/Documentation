0d4093e2c9bfebc317a78e8124e3d5b60e492dd9
Lio Gir <giraud.lio.pro@gmail.com>
2018-09-01 13:30:55 +0100

    Exercices de Java avant de passer aux specificités Java8

---
+++ Language/Java8/workspaceCoursJava8/projet1/src/projet1/test.java
@@ -0,0 +1,18 @@
+package projet1;
+
+import java.util.Scanner;
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Veuillez saisir un mombreot :");
+
+		int str = sc.nextInt();
+
+		System.out.println("Vous avez saisi : " + str);
+
+	}
+
+}
