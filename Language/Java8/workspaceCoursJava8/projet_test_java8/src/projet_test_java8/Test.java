0b7d8303ac0e969bf44b236eca5dd307e06a2645
Lio Gir <giraud.lio.pro@gmail.com>
2018-09-14 07:41:29 +0100

    Premiers tests java 8 - durée

--- Language/Java8/workspaceCoursJava8/projet_test_java8/src/projet_test_java8/Test.java
+++ Language/Java8/workspaceCoursJava8/projet_test_java8/src/projet_test_java8/Test.java
@@ -1,12 +1,11 @@
 package projet_test_java8;
 
+import java.time.Duration;
 import java.time.LocalDate;
-
 import java.time.LocalDateTime;
-
-import java.time.LocalTime;
-
 import java.time.Month;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
 
 
 public class Test {
@@ -71,7 +70,24 @@ public class Test {
         
 
       System.out.println("Mois : " + moisPars +", jour : " + jourPars +", heure : " + heurePars);
+      
+      
+      LocalDateTime ldt = LocalDateTime.of(2018, Month.DECEMBER, 25, 13, 37, 0);
+      LocalDateTime ldt2 = ldt.plus(3, ChronoUnit.YEARS);
+      LocalDateTime ldt3 = ldt.minusMinutes(1337);
 
+      Period p = Period.between(ldt.toLocalDate(), ldt2.toLocalDate());
+      Duration d = Duration.between(ldt.toLocalTime(), ldt3.toLocalTime());
+      System.out.println("Période : " + p);
+      System.out.println("Période en jours : " + p.getDays());
+      System.out.println("Période en mois : " + p.getMonths());
+      System.out.println("Période en année : " + p.getYears());
+      System.out.println("Période en jours : " + p.get(ChronoUnit.DAYS));
+      System.out.println("Durée : " + d.getSeconds());
+      System.out.println("Ecart en jour : " + ChronoUnit.DAYS.between(ldt, ldt2));
+      System.out.println("Ecart en minutes : " + ChronoUnit.MINUTES.between(ldt, ldt2));
+      System.out.println("Ecart en mois : " + ChronoUnit.MONTHS.between(ldt, ldt2));
+      System.out.println("Ecart en années : " + ChronoUnit.YEARS.between(ldt, ldt2));
     }
 
 }
